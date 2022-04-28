00205b1d12806606820286996a855d4e03440629
Gurcan <53661349+egecangurcan@users.noreply.github.com>
2022-04-28 17:19:45 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
+first = lambda n: (n-3)/n**2
 
-name = input("What is your name?")
-print("Hello,", name)
-StudentId = input("What is your Student ID?")
-print("Hello again,your student ID is:", StudentId)
+userinput = int(input("Please enter a number as (n)"))
+list = []
+for i in range(1,userinput+1):
+    list.append(first(i))
+
+print(sum(list))
 
 
 
-num1 = int(input("Enter a number here:"))
-num2 = int(input("Enter a number again:"))
-Sum = num2 + num1
-Diff = abs(num2 - num1)
-Prod = num2 * num1
-
-print(Sum ," ", Diff, " " , Prod)
-print("*\n"
-      "**\n"
-      "***\n")
+def rec(n):
+    if(n == 1):
+        return (n/(n+2)) - 10
+    else:
+        return rec(n-1) * (n/(n+2)- 10)
+print(rec(2))
 
